0b51aecf8f771b2efb2e82185eba41e78755cb44
DESKTOP-11LG58E\Oscar <operezcham@gmail.c0m>
2023-10-20 17:44:33 -0600

    🔒

--- app/Controllers/Http/HomeController.js
+++ app/Controllers/Http/HomeController.js
@@ -1,10 +1,7 @@
 class HomeController {
     async index({ auth, view }) {
         const user = await auth.getUser()
-        /*return view.render('welcome', {
-            user
-        })*/
-        return { user };
+        return view.render('welcome', { username: user.username })
     }
 }
 
